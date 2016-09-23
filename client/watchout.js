39daed73c6989c978275f618df357e2f7075e7a9
bantalek <benantalek@gmail.com>
2016-09-23 15:02:53 -0700

    Implement update of circle objects.

--- client/watchout.js
+++ client/watchout.js
@@ -5,12 +5,16 @@ var highScore = 0;
 
 var svg = d3.select('svg');
 
-var circles = d3.range(10).map(function() {
-  return {
-    x: Math.floor(Math.random() * 600),
-    y: Math.floor(Math.random() * 500),
-  };
-});
+var makeCircles = function() {
+  return d3.range(10).map(function() {
+    return {
+      x: Math.floor(Math.random() * 600),
+      y: Math.floor(Math.random() * 500),
+    };
+  });
+};
+
+var circles = makeCircles();
 
 //start by placing 10 randomly placed circles onto the board
 svg.selectAll('circle')
@@ -24,20 +28,25 @@ svg.selectAll('circle')
 
 var dragged = function(d) {
   //make sure the mouse is in bounds horizontally
-  if (d3.event.x <= 15 || d3.event.x >= 585) {
+  var mouseX = d3.event.x;
+  var mouseY = d3.event.y;
+  if (mouseX <= 15 || mouseX >= 585) {
     return;
   }
   //make sure the mouse is in bounds vertically
-  if (d3.event.y <= 15 || d3.event.y >= 485) {
+  if (mouseY <= 15 || mouseY >= 485) {
     return;
   }
 
   //check for collisions
   var checkCollisions = function() {
-    var asteroids = svg.selectAll('.asteroid');
-    console.log(asteroids);
+    var asteroids = d3.select('svg').selectAll('.asteroid');
     for (var i = 0; i < asteroids[0].length; i++) {
-      console.log(asteroids[0][i].cx);
+      var asteroidX = asteroids[0][i].cx.baseVal.value;
+      var asteroidY = asteroids[0][i].cy.baseVal.value;
+      if ((Math.abs(asteroidX - mouseX) <= 15) && (Math.abs(asteroidY - mouseY) <= 15)) {
+        console.log('crash');
+      }
     }
   };
 
@@ -51,6 +60,34 @@ var drag = d3.behavior.drag().on('drag', dragged);
 d3.select('.mouse')
   .call(drag);
 
-var timer = d3.timer(function(elapsed) {
+var update = function(data) {
+  console.log('!');
+  var svg = d3.select('svg').selectAll('circle').data(data, function(d) { return d.x + d.y; });
 
-}, 1000);
+  //exit old elements not present in new data
+  svg.exit()
+    .remove();
+
+  //Update old elements present in new data
+  svg.attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .attr('r', 15)
+    .classed('asteroid', true)
+    .style('fill', 'white');
+
+  //enter new elements
+  svg.enter().append('circle')
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .attr('r', 15)
+    .classed('asteroid', true)
+    .style('fill', 'white');
+
+
+
+};
+
+var timer = d3.timer(function(elapsed) {
+  var newCircles = makeCircles();
+  update(newCircles);
+}, 500);
